32d0319bda4c517715ce0ec2501b6d7ef82796ab
Tyrel Datwyler <tyreld@linux.vnet.ibm.com>
2019-04-29 22:17:23 -0500

    Update setup.sh to checkout standard ppc64 dev repos

--- setup.sh
+++ setup.sh
@@ -2,3 +2,14 @@
 
 ln -ivs $(pwd)/gitconfig ~/.gitconfig
 ln -ivs $(pwd)/git_aliases ~/.git_aliases
+
+pushd ~
+git clone-linus
+pushd linux
+git add-tyreld
+git fetch tyreld
+popd
+git clone-ppcutils
+git clone-librtas
+git clone-diag
+popd
